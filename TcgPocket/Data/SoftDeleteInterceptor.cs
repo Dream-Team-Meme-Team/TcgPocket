4883575d27668cc996e83ce80460785972e46771
alluna <bbarr5160@gmail.com>
2023-08-25 18:42:05 -0500

    feat: register services and gitignore

---
+++ TcgPocket/Data/SoftDeleteInterceptor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace TcgPocket.Data;
+
+public class SoftDeleteInterceptor : SaveChangesInterceptor
+{
+    public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+    {
+        if (eventData.Context is null) return result;
+
+        foreach (var entry in eventData.Context.ChangeTracker.Entries())
+        {
+            // TODO: things and stuff maybe, idk this is just here for something
+        }
+
+        return result;
+    }
+}
---
+++ TcgPocket/Startup.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using TcgPocket.Data;
+using TcgPocket.Features;
+
+namespace TcgPocket;
+
+public class Startup
+{
+    private IConfigurationRoot _configuration { get; }
+
+    public Startup()
+    {
+        var builder = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json");
+
+        _configuration = builder.Build();
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddSingleton(_configuration);
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<Startup>());
+        services.AddAutoMapper(typeof(IMapper));
+        services.AddValidatorsFromAssemblyContaining<IValidator>(ServiceLifetime.Scoped);
+        services.AddDbContext<DataContext>(options =>
+            options.UseSqlServer(_configuration.GetConnectionString(AppSettings.DefaultConnection)));
+    }
+}
