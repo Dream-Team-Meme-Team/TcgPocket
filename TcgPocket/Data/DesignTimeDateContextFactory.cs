c31cf17fd33a5ceff46cd16919b3b292c51f2428
Deanna <deannacshort@gmail.com>
2023-09-03 00:04:27 -0500

    fix: connection string

--- TcgPocket/Data/DesignTimeDateContextFactory.cs
+++ TcgPocket/Data/DesignTimeDateContextFactory.cs
@@ -8,8 +8,14 @@ public class DesignTimeDataContextFactory : IDesignTimeDbContextFactory<DataCont
 {
     public DataContext CreateDbContext(string[] args)
     {
+        string projectPath = AppDomain.CurrentDomain.BaseDirectory.Split(new String[] { @"bin\" }, StringSplitOptions.None)[0];
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(projectPath)
+            .AddJsonFile("appsettings.json")
+            .Build();
+        string connectionString = configuration.GetConnectionString(AppSettings.DefaultConnection);
         var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
-        optionsBuilder.UseSqlServer(AppSettings.DefaultConnection);
+        optionsBuilder.UseSqlServer(connectionString);
         // optionsBuilder.AddInterceptors(new SoftDeleteInterceptor());
 
         return new DataContext(optionsBuilder.Options);
