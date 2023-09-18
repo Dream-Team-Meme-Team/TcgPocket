e29ac54c168252b5b0a2682333e9fb8dbf72fce3
Deanna <deannacshort@gmail.com>
2023-09-18 01:39:42 -0500

    wip: pagination

---
+++ TcgPocket/Shared/Queries/PagedResultClass.cs
@@ -0,0 +1,114 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace TcgPocket.Shared.Queries;
+
+public static class PagedResultClass
+{
+    public static IMapper mapper;
+    public static PagedResult<T> GetPaged<T>(this IQueryable<T> query, int page, int pageSize)
+    {
+        var result = new PagedResult<T>
+        {
+            CurrentPage = page,
+            PageSize = pageSize,
+            RowCount = query.Count()
+        };
+
+        var pageCount = (double)result.RowCount / pageSize;
+        result.PageCount = (int)Math.Ceiling(pageCount);
+
+        var skip = (page - 1) * pageSize;
+        result.Results = query.Skip(skip).Take(pageSize).ToList();
+
+        return result;
+    }
+
+    public static async Task<PagedResult<T>> GetPagedAsync<T>(this IQueryable<T> query, int page, int pageSize)
+    {
+        var result = new PagedResult<T>
+        {
+            CurrentPage = page,
+            PageSize = pageSize,
+            RowCount = await query.CountAsync()
+        };
+
+        var pageCount = (double)result.RowCount / pageSize;
+        result.PageCount = (int)Math.Ceiling(pageCount);
+
+        var skip = (page - 1) * pageSize;
+        result.Results = await query.Skip(skip).Take(pageSize).ToListAsync();
+
+        return result;
+    }
+
+    public static PagedResult<U> GetPaged<T, U>(this IQueryable<T> query, int page, int pageSize) where U : class
+    {
+        var result = new PagedResult<U>();
+        result.CurrentPage = page;
+        result.PageSize = pageSize;
+        result.RowCount = query.Count();
+
+        var pageCount = (double)result.RowCount / pageSize;
+        result.PageCount = (int)Math.Ceiling(pageCount);
+
+        var skip = (page - 1) * pageSize;
+        result.Results = query.Skip(skip)
+                              .Take(pageSize)
+                              .ProjectTo<U>(mapper.ConfigurationProvider)
+                              .ToList();
+        return result;
+    }
+
+    public static async Task<PagedResult<U>> GetPagedAsync<T, U>(this IQueryable<T> query, int page, int pageSize) where U : class
+    {
+        var result = new PagedResult<U>();
+        result.CurrentPage = page;
+        result.PageSize = pageSize;
+        result.RowCount = await query.CountAsync();
+
+        var pageCount = (double)result.RowCount / pageSize;
+        result.PageCount = (int)Math.Ceiling(pageCount);
+
+        var skip = (page - 1) * pageSize;
+        result.Results = await query.Skip(skip)
+                                    .Take(pageSize)
+                                    .ProjectTo<U>(mapper.ConfigurationProvider)
+                                    .ToListAsync();
+        return result;
+    }
+
+    public class PagedResult<T> : PagedResultBase
+    {
+        public IList<T> Results { get; set; }
+
+        public PagedResult()
+        {
+            Results = new List<T>();
+        }
+    }
+}
+
+public abstract class PagedResultBase
+{
+    public int CurrentPage { get; set; }
+
+    public int PageCount { get; set; }
+
+    public int PageSize { get; set; }
+
+    public int RowCount { get; set; }
+    public string LinkTemplate { get; set; }
+
+    public int FirstRowOnPage
+    {
+
+        get { return (CurrentPage - 1) * PageSize + 1; }
+    }
+
+    public int LastRowOnPage
+    {
+        get { return Math.Min(CurrentPage * PageSize, RowCount); }
+    }
+}
