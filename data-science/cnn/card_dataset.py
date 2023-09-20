0da6d0f22955035682179d1da334b2e077be3300
SpicyNova <abby.ortego@selu.edu>
2023-09-19 22:49:33 -0500

    unbroken data set and ready to train

--- data-science/cnn/card_dataset.py
+++ data-science/cnn/card_dataset.py
@@ -30,24 +30,24 @@ class CardDataSet(Dataset):
 
     def __getitem__(self, indx):
         
-        try:
-            # get and load up card from URL
-            resp = requests.get(self.df.iloc[indx, 0], headers = {'X-Api-Key': IDK_API_KEY, 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'})
-            img = np.array(Image.open(BytesIO(resp.content)))
+        # try:
+        # get and load up card from URL
+        resp = requests.get(self.df.iloc[indx, 0], headers = {'X-Api-Key': IDK_API_KEY, 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'})
+        img = np.array(Image.open(BytesIO(resp.content)))
 
-            # rearrange color channels >>> RGBA to BGR, RGB to BGR, etc...
-            if img.shape[2] == 4:
-                img = cv2.resize(cv2.cvtColor(img, cv2.COLOR_BGRA2BGR), [421, 614])
-            else:
-                img = cv2.resize(cv2.cvtColor(img, cv2.COLOR_RGB2BGR), [421, 614])
-            
-            # convert to pytorch tensor (via PIL Image) and normalize
-            tensor = from_PIL(Image.fromarray(np.uint8(img))).to(torch.float32)
-            data = [(tensor / torch.max(tensor)), self.df.iloc[indx, 1]]
+        # rearrange color channels >>> RGBA to BGR, RGB to BGR, etc...
+        if img.shape[2] == 4:
+            img = cv2.resize(cv2.cvtColor(img, cv2.COLOR_BGRA2BGR), [421, 614])
+        else:
+            img = cv2.resize(cv2.cvtColor(img, cv2.COLOR_RGB2BGR), [421, 614])
+        
+        # convert to pytorch tensor (via PIL Image) and normalize
+        tensor = from_PIL(Image.fromarray(np.uint8(img))).to(torch.float32)
+        data = [(tensor / torch.max(tensor)), self.df.iloc[indx, 1]]
         #
-        except:
-            pass
-        #
+        # except:
+        #     pass
+        # #
 
         return data
     #
