4943e5b89eb09eb5132201c966de3195e7660017
Deanna Short <deannacshort@gmail.com>
2023-09-27 23:01:02 -0500

    feat: backend change user delete to detect user and take in a password

---
+++ TcgPocket/Features/Users/Validators/UserDeleteDtoValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using System.Text.RegularExpressions;
+using TcgPocket.Features.Users.Dtos;
+
+namespace TcgPocket.Features.Users.Validators;
+
+public class UserDeleteDtoValidator : AbstractValidator<UserDeleteDto>
+{
+    public UserDeleteDtoValidator()
+    {
+        RuleFor(x => x.Password)
+            .MaximumLength(25)
+            .NotEmpty();
+
+        RuleFor(x => x.ConfirmPassword)
+            .Cascade(CascadeMode.Stop)
+            .NotEmpty()
+            .Equal(x => x.Password)
+            .WithMessage("Passwords do not match.");
+    }
+}
