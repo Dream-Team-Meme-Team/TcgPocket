589eedd9ef92cbacdcb72c00f03838cdeeab4e64
SpicyNova <abby.ortego@selu.edu>
2023-10-02 23:10:34 -0500

    WIP: reading yugioh

--- data-science/scraper/main.py
+++ data-science/scraper/main.py
@@ -14,32 +14,49 @@ from pokemon_scraper import PokemonScraper
 from_PIL = transform.Compose([transform.PILToTensor()]) 
 
 # """MAGIC"""
-# # get and load up card from URL
-# resp_mgc = requests.get('https://cards.scryfall.io/normal/front/e/5/e5594689-0f32-4592-859b-c746f3464fc6.jpg?1573509272', headers = {'X-Api-Key': IDK_API_KEY, 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'})
-# img_mgc_raw = Image.open(BytesIO(resp_mgc.content))
-# img_mgc_raw.show()
+# # # get and load up card from URL
+# # resp_mgc = requests.get('https://cards.scryfall.io/normal/front/e/5/e5594689-0f32-4592-859b-c746f3464fc6.jpg?1573509272', headers = {'X-Api-Key': IDK_API_KEY, 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'})
+# # img_mgc_raw = Image.open(BytesIO(resp_mgc.content))
+# # img_mgc_raw.show()
 
-# # img_mgc_raw = Image.open('data-science/data/collected/card.jpg')
+# img_mgc_raw = Image.open('data-science/data/collected/card.jpg')
 
 # mgc_scrape = MagicScraper()
 # filt = mgc_scrape.apply_filter(img_mgc_raw)
 # print(mgc_scrape.read_card(filt))
 
 
-# """YUGIOH"""
-# # get and load up ygo card from URL
+"""YUGIOH"""
+# get and load up ygo card from URL
 # resp_ygo = requests.get('https://images.ygoprodeck.com/images/cards/13026402.jpg', headers = {'X-Api-Key': IDK_API_KEY, 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'})
 # img_ygo_raw = Image.open(BytesIO(resp_ygo.content))
 # img_ygo_raw.show()
 
-# img_ygo_np = np.array(img_ygo_raw.resize([421,614]))
+img_ygo_raw = Image.open('data-science/data/collected/card4.jpg')
+
+# ygo_scrape = YugiohScraper()
+# filt = ygo_scrape.apply_filter(img_ygo_raw)
+# print(ygo_scrape.read_card(filt))
+
+img_ygo_np = np.array(img_ygo_raw.resize([421,614]))[20:70, 30:330, :]
 # # img_ygo_np[440:455, 300:380, :] = 0
 # img_ygo_np[:435, :, :] = 0
 # img_ygo_np[460:, :, :] = 0
 # img_ygo_np[:, :295, :] = 0
 # img_ygo_np[:, 385:, :] = 0
-# img_ygo = Image.fromarray(img_ygo_np)
-# img_ygo.show()
+# img_ygo_np = cv2.blur(img_ygo_np, (3,3))
+img_ygo = Image.fromarray(img_ygo_np)
+img_ygo.show()
+
+# # kernel = np.array([[0, 1, 0],
+# #                    [1, 1, 1],
+# #                    [0, 1, 0]], np.uint8)
+
+# img_ygo = cv2.threshold(cv2.cvtColor(img_ygo_np, cv2.COLOR_BGR2GRAY), 29, 255, cv2.THRESH_BINARY_INV)[1]
+# # img_ygo = cv2.dilate(cv2.threshold(cv2.cvtColor(img_ygo_np, cv2.COLOR_BGR2GRAY), 31, 255, cv2.THRESH_BINARY_INV)[1], kernel, iterations=1)
+# cv2.imshow('', img_ygo)
+# cv2.waitKey(0)
+print(tess.image_to_string(img_ygo, config="--psm 13 -c tessedit_char_whitelist=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ").strip('\n'))
 
 
 # """POKEMON"""
--- data-science/scraper/yugioh_scraper.py
+++ data-science/scraper/yugioh_scraper.py
@@ -1,2 +1,30 @@
+import numpy as np
+import pytesseract as tess
+
 class YugiohScraper:
-    pass
+
+    def apply_filter(self, raw_card):
+        """
+        Arguments:
+            raw_card: user uploaded image of card
+            return: one cropped img containing the unique code
+        """
+        code = np.array(raw_card.resize([421,614]))[435:460, 295:385, :]
+        
+        return code
+    
+    def read_card(self, filt_cards: list):
+        """
+        Arguments:
+            filt_cards: list of cropped images containing the values needed to be read
+            return: the read values from list of cropped images
+        """
+
+        params = []
+        for card in filt_cards:
+            params.append(tess.image_to_string(card).strip('\n'))
+        
+        return params
+
+    def gen_query(self):
+        pass
