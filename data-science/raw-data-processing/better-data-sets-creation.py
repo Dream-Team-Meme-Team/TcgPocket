752ced4ccdc529e4a9274a5b705f3c868ab5b9f8
Abby Ortego <89620898+abbyortego@users.noreply.github.com>
2023-10-02 23:14:26 -0500

    CNN Architecture & Train (#31)
    * official raw labeled data
    
    * created train and test data sets with labels
    
    * file struct renaming
    
    * WIP loading card imgs from url
    
    * Grabbing pokemon cards via URL frickin works now WHOOP
    
    * loading & cleaning data
    
    * adding disclaimers
    
    * WIP: cnn class
    
    * WIP: cnn class
    
    finally figuring out how pytorch works
    
    * WIP: cnn class
    
    *TWO* convolutions - crazy ik
    
    * getting familiar with pytorch
    
    final commit before blowing up data science repo
    
    * getting familiar with pytorch
    
    final commit before blowing up data science repo
    
    * data set is finally loading >:)
    
    things are coming together
    
    * final touches for data loading
    
    * final commit before CNN training / testing phase
    
    * i lied, this is the final commit before training CNN
    
    i was wrong about a lot of things and it all needed to be fixed at 2am
    
    * tweehee i lied again, more last minute updates
    
    CNN training will commence soon (i pinky promise)
    
    * i am not a woman of my word
    
    * hiding api key...again >:)
    
    * unbroken data set and ready to train
    
    * smol data set training
    
    limits on api hits make brain go grrrr
    
    * model's v1
    
    * model v2
    
    * snippet of tsting CNN out
    
    * model v3
    
    * 97.9% ACCURRACT AHH
    
    * some file restructuring
    
    * touch ups
    
    * model av2
    
    * file restructuring and naming conventions
    
    * deleting commented layer from lv

---
+++ data-science/raw-data-processing/better-data-sets-creation.py
@@ -0,0 +1,88 @@
+# %% IMPORTS
+from torch.utils.data import Dataset
+import pandas as pd
+import torch
+import requests
+import numpy as np
+from PIL import Image
+import cv2 
+from io import BytesIO
+import torchvision.transforms as transform
+
+# %% LOAD CSV - TRAIN
+train_df = pd.read_csv('C:/Users/abbyo/Documents/PROJECTS/TcgPocket/data-science/data/train/data.txt')
+display(train_df)
+
+# %% START WEEDING THEM OUT
+from_PIL = transform.Compose([transform.PILToTensor()]) 
+bad_indx = []
+
+for indx in range(len(train_df)):
+    print(f'{indx}/{len(train_df)}')
+    if train_df.iloc[indx, 1] == 2:
+        try:
+            # get and load up card from URL
+            resp = requests.get(train_df.iloc[indx, 0], headers = {'X-Api-Key': '7ccb4c32-6299-4533-bf47-36f4d2a95117', 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'})
+            img = np.array(Image.open(BytesIO(resp.content)))
+
+            # rearrange color channels >>> RGBA to BGR, RGB to BGR, etc...
+            if img.shape[2] == 4:
+                img = cv2.resize(cv2.cvtColor(img, cv2.COLOR_BGRA2BGR), [421, 614])
+            else:
+                img = cv2.resize(cv2.cvtColor(img, cv2.COLOR_RGB2BGR), [421, 614])
+            
+            # convert to pytorch tensor (via PIL Image) and normalize
+            tensor = from_PIL(Image.fromarray(np.uint8(img))).to(torch.float32)
+            data = [(tensor / torch.max(tensor)), train_df.iloc[indx, 1]]
+        #
+        except:
+            print('Exception Occurred')
+            bad_indx.append(indx)
+        #
+#
+
+# %% DELETE 'EM
+train_df = train_df.drop(index=bad_indx)
+print(len(train_df))
+
+# %% OVERWRITE 'EM
+train_df.to_csv('C:/Users/abbyo/Documents/PROJECTS/TcgPocket/data-science/data/train/data.txt', index=False)
+
+# %% LOAD CSV - TEST
+test_df = pd.read_csv('C:/Users/abbyo/Documents/PROJECTS/TcgPocket/data-science/data/test/data.txt')
+display(test_df)
+
+# %% START WEEDING THEM OUT
+from_PIL = transform.Compose([transform.PILToTensor()]) 
+bad_indx_2 = []
+
+for indx in range(len(test_df)):
+    print(f'{indx}/{len(test_df)}')
+    if test_df.iloc[indx, 1] == 2:
+        try:
+            # get and load up card from URL
+            resp = requests.get(test_df.iloc[indx, 0], headers = {'X-Api-Key': '''hidden''', 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'})
+            img = np.array(Image.open(BytesIO(resp.content)))
+
+            # rearrange color channels >>> RGBA to BGR, RGB to BGR, etc...
+            if img.shape[2] == 4:
+                img = cv2.resize(cv2.cvtColor(img, cv2.COLOR_BGRA2BGR), [421, 614])
+            else:
+                img = cv2.resize(cv2.cvtColor(img, cv2.COLOR_RGB2BGR), [421, 614])
+            
+            # convert to pytorch tensor (via PIL Image) and normalize
+            tensor = from_PIL(Image.fromarray(np.uint8(img))).to(torch.float32)
+            data = [(tensor / torch.max(tensor)), test_df.iloc[indx, 1]]
+        #
+        except:
+            print('Exception Occurred')
+            bad_indx_2.append(indx)
+        #
+#
+
+# %% DELETE 'EM
+test_df = test_df.drop(index=bad_indx_2)
+print(len(test_df))
+
+# %% OVERWRITE 'EM
+test_df.to_csv('C:/Users/abbyo/Documents/PROJECTS/TcgPocket/data-science/data/test/data.txt', index=False)
