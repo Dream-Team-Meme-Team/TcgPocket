adbb93648b99e5d9f37f4d80b14e05516271e30a
Brandon Barr <92871497+xalluna@users.noreply.github.com>
2023-10-20 10:42:14 -0500

    jessie is not her name :upside_down:
    im sorry abby i didn't mean to give av the wrong name

--- TcgPocket/testmodel.py
+++ TcgPocket/testmodel.py
@@ -1,17 +1,13 @@
 import base64
 from sys import argv
-import datascience.main as jessie
+import datascience.main as av
 
 card = 'datascience/1.jpg'
 
 if __name__ == '__main__':
     try:
         with open(card, "rb") as image_file:
-            byte_array = bytearray(image_file.read())
-            byte_string = base64.b64encode(byte_array).decode('utf-8')
-            byte_array = bytearray(base64.b64decode(byte_string))
-            json = jessie.all_of_it(byte_array)
-
+            json = av.all_of_it(bytearray(image_file.read()))
             print(json)
 
     except Exception as e:
