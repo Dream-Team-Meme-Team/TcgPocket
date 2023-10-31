738836110392948f96cc6c59c3083660d561b3c5
SpicyNova <abby.ortego@selu.edu>
2023-10-30 20:48:58 -0500

    WIP: magic web scraping
    i'm not too sure of the lagality of this but we're getting price data

---
+++ TcgPocket/datascience/scraper/magic_scraper.py
@@ -0,0 +1,55 @@
+#%% IMPORTS
+import requests
+from bs4 import BeautifulSoup as bs
+
+# %% FUNCTIONS
+base = 'https://www.pricecharting.com'
+
+def get_sets(url: str) -> list:
+
+    resp = requests.get(url)
+    soup = bs(resp.content, 'html.parser')
+    sets = soup.find('div', class_='home-box all').ul.find_all('li')
+
+    return {set.text.strip('\n'): base + set.a.get('href') for set in sets}
+#
+
+def get_cards(url: str):
+    resp = requests.get(url)
+    soup = bs(resp.content, 'html.parser')
+    cards = soup.find('table', id='games_table').tbody.find_all('td', class_='title')
+
+    return {card.text.strip('\n'): base + card.a.get('href') for card in cards}
+#
+
+def get_prices(url: str):
+
+    try:
+        resp = requests.get(url)
+        soup = bs(resp.content, 'html.parser')
+        card_dates = soup.find('table', class_='hoverable-rows sortable').tbody.find_all('td', class_='date')
+        card_prices = soup.find('table', class_='hoverable-rows sortable').tbody.find_all('td', class_='numeric')
+        print([date.text for date in card_dates], [price.span.text for price in card_prices])
+        # return {price.span.text for price in cards}
+    except:
+        pass
+    #
+
+    return
+#
+
+# %% SET LINKS
+set_links = get_sets('https://www.pricecharting.com/category/magic-cards')
+print(set_links)
+
+# %% CARD LINKS
+for name, link in set_links.items():
+    set_links[name] = get_cards(link)
+#
+print(set_links)
+
+# %% CARD PRICES
+for set, cards in set_links.items():
+    for card, link in cards.items():
+        # print(card, get_prices(link))
+        print(get_prices(link))
