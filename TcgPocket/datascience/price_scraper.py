3c0429a986c6f64c5e6175ca33034f8b77e03ff5
SpicyNova <abby.ortego@selu.edu>
2023-11-03 01:20:12 -0500

    beeg price scraping script all done

--- TcgPocket/datascience/price_scraper.py
+++ TcgPocket/datascience/price_scraper.py
@@ -1,26 +1,36 @@
 import requests
 from bs4 import BeautifulSoup as bs
+from collections import defaultdict
 
 
 def price_scraper(game: str):
     set_link = 'https://www.pricecharting.com/category/' + game + '-cards'
     base = 'https://www.pricecharting.com'
 
-    # grab link to list of cards in set
     set_resp = bs(requests.get(set_link).content, 'html.parser')
-    # data = {set.text.strip('\n'): base + set.a.get('href') for set in set_resp.find('div', class_='home-box all').ul.find_all('li')}
-    
-    # # grab link to price listings for cards in sets
-    # for set, link in data.items():
-    #     card_resp = bs(requests.get(link).content, 'html.parser')
-    #     data[set] = {card.text.strip('\n, Magic '): base + card.a.get('href') for card in card_resp.find('table', id='games_table').tbody.find_all('td', class_='title')}
-    #     print(data[set])
-    # #
+
+    data = defaultdict(dict)
 
     for set in set_resp.find('div', class_='home-box all').ul.find_all('li'):
-        print(base + set.a.get('href'))
+        card_resp = bs(requests.get(base + set.a.get('href')).content, 'html.parser')
 
-    # print(data)
+        for card in card_resp.find('table', id='games_table').tbody.find_all('td', class_='title'):
+            prices = bs(requests.get(base + card.a.get('href')).content)
+            
+            try:
+                card_dates = prices.find('table', class_='hoverable-rows sortable').tbody.find_all('td', class_='date')
+                card_prices = prices.find('table', class_='hoverable-rows sortable').tbody.find_all('td', class_='numeric')
+
+                data[set.text.strip('\n')][card.text.strip('\n')] = dict([(date.text, float(price.span.text.strip('$'))) for  i, (date, price) in enumerate(zip(card_dates, card_prices))])
+            except:
+                data[set.text.strip('\n')][card.text.strip('\n')] = None
+            #
+        #
+
+        print(data[set.text.strip('\n')])
+    #
+
+    return data
 #
 
-price_scraper('pokemon')
+print(price_scraper('magic'))
