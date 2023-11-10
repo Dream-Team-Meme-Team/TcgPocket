f902603840f25c728b4131de46feaeaf4c04603b
Deanna Short <deannacshort@gmail.com>
2023-11-10 02:11:19 -0600

    almost forgot: clear on page leave

--- TcgPocket/App/src/pages/inventory/InventoryPage.tsx
+++ TcgPocket/App/src/pages/inventory/InventoryPage.tsx
@@ -72,6 +72,7 @@ export function InventoryPage(): React.ReactElement {
 
   useEffect(() => {
     dispatch(resetFilters());
+    dispatch(setSearchTextInventory(''));
   }, [selectedGame]);
 
   useEffect(() => {
