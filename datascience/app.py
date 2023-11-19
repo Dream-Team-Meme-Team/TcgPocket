7d29b43e64a3679ae74ed23e3e5cbe69067049cc
alluna <bbarr516@gmail.com>
2023-11-19 17:21:06 -0600

    fix: make origins a env variable

--- datascience/app.py
+++ datascience/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_cors import CORS
 from flask_config.config import Config
@@ -5,7 +6,7 @@ from flask_config.entities.entities import db
 from flask_config.routes.card_reader_routes import card_reader_bp
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*", "supports_credentials": True}})
+CORS(app, resources={r"/*": {"origins": os.environ.get('ORIGINS'), "supports_credentials": True}})
 app.config.from_object(Config)
 
 db.init_app(app)
