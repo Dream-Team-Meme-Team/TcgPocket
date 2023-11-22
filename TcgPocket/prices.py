73560adf023f98bf9fcd780fd8d2204218388af6
SpicyNova <abby.ortego@selu.edu>
2023-11-21 23:34:56 -0600

    upload blob work

---
+++ TcgPocket/prices.py
@@ -0,0 +1,35 @@
+from azure.storage.blob import BlobServiceClient, ContentSettings
+import json
+import datascience.price_scraper as scraper
+
+def upload_json_to_blob(json_data, container_name, blob_name, connection_string):
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+
+    # Convert JSON data to bytes
+    json_bytes = json.dumps(json_data, indent=2).encode('utf-8')
+
+    # Upload the JSON data to the blob
+    blob_client.upload_blob(json_bytes, content_settings=ContentSettings(content_type='application/json'), overwrite=True)
+
+# Example usage
+# json_data_to_upload = {'magic': {
+#                             'Kaldheim': {
+#                                 'Lathril': {
+#                                     '08-22-2021': 2.99,
+#                                     '08-30-2021': 2.99,
+#                                     '09-8-2021': 2.95
+#                                 },
+#                                 'Abomination of Lanowar': {
+#                                     '06-12-2021': 5.02,
+#                                     '06-23-2021': 4.99,
+#                                     '06-30-2021': 4.98
+#                                 }
+#                             }
+#                         }}
+json_data_to_upload = scraper.update_prices('magic')
+container_name = 'price-data'
+blob_name = 'magic-prices.json'
+connection_string = 'AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;'
+
+upload_json_to_blob(json_data_to_upload, container_name, blob_name, connection_string)
