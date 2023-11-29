5c60379862af175a2ab6cd8771b7362d38a8dfdf
Deanna Short <deannacshort@gmail.com>
2023-11-29 15:29:18 -0600

    fix console error

--- TcgPocket/App/src/pages/deck/modules/AllDecksTab.tsx
+++ TcgPocket/App/src/pages/deck/modules/AllDecksTab.tsx
@@ -67,7 +67,7 @@ export const AllDecksTab: React.FC<DeckTabProps> = ({ decks, loading }) => {
   return (
     <div>
       {deckDisplayOrder.map((gameAndDecks, index) => (
-        <>
+        <div key={gameAndDecks.game.id}>
           {displayIsVisible(gameAndDecks.game.id) && (
             <DeckListingDisplay
               data={gameAndDecks.decks}
@@ -78,7 +78,7 @@ export const AllDecksTab: React.FC<DeckTabProps> = ({ decks, loading }) => {
               key={index}
             />
           )}
-        </>
+        </div>
       ))}
     </div>
   );
