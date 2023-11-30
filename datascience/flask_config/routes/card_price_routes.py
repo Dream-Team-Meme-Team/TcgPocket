5d8a8741784df87d19a99d30d721934a8fbf523d
SpicyNova <abby.ortego@selu.edu>
2023-11-29 19:38:07 -0600

    final commit
    it's all over

--- datascience/flask_config/routes/card_price_routes.py
+++ datascience/flask_config/routes/card_price_routes.py
@@ -3,8 +3,6 @@ from flask_config.utilities.price_scraper import scrape_prices
 from flask_config.utilities.price_blob import upload_json_to_blob, download_json_from_blob
 from dotenv import load_dotenv
 import os
-
-import plotly
 import plotly.express as px
 import pandas as pd
 
@@ -26,8 +24,24 @@ def update_prices(game:str) -> str:
     return f"{game} prices stored in blob successfully!\n"
 #
 
+@card_pricer_bp.route('/api/get-prices/<game>', methods = ['GET'])
+def get_game_prices(game:str) -> dict:
+    blob_name = f'{game}-prices.json'
+    data = download_json_from_blob(container_name, blob_name, connection_string)
+
+    return data
+#
+
+@card_pricer_bp.route('/api/get-prices/<game>/<set>', methods = ['GET'])
+def get_set_prices(game:str, set:str) -> dict:
+    blob_name = f'{game}-prices.json'
+    data = download_json_from_blob(container_name, blob_name, connection_string)
+
+    return data[set]
+#
+
 @card_pricer_bp.route('/api/get-prices/<game>/<set>/<card>', methods = ['GET'])
-def get_prices(game:str, set:str, card:str) -> dict:
+def get_card_prices(game:str, set:str, card:str) -> dict:
     blob_name = f'{game}-prices.json'
     data = download_json_from_blob(container_name, blob_name, connection_string)
     df = pd.DataFrame.from_dict(data[set][card], orient='index', columns=['Price (USD)'])
